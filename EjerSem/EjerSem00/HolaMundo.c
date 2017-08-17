e2d8988eb70c566c321f246abce06f36537da890
Argenis616 <argenis616@ciencias.unam.mx>
2017-08-17 15:11:48 -0500

    Creacion de un hola Mundo en C

---
+++ EjerSem/EjerSem00/HolaMundo.c
@@ -0,0 +1,8 @@
+/**Este pequeño progrma nos muestra un hola mundo en la terminal*/
+#include <stdio.h>
+
+int main()
+{
+        printf("Hola mundo");
+        return 0;
+}
---
+++ EjerSem/EjerSem00/Holamundo.pas
@@ -0,0 +1,5 @@
+{Es un pequeño programa que nos enseña un hola mundo}
+program Hello;
+   begin     
+      writeln ('Hola mundo')
+   end.      
